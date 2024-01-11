543bdfa494d463e16c5bccd1932f94fe141c102a
AnnaZiankevich <anna.ziankevich@gmail.com>
2024-01-12 01:16:30 +0300

    Delete unneeded files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import '../src/styles/_main.scss'
 import Header from './components/header/Header';
 import MainComponent from './components/main-component/MainComponent.jsx';
-import './app.scss'
 
 function App() {
   return (
