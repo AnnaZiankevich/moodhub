38ef8ead1dd21cdbbac91c97a56f7af70903c9c1
AnnaZiankevich <anna.ziankevich@gmail.com>
2024-01-13 00:09:33 +0300

    Add redux, edit TableRecord component
    Also MyDiary layout changes

--- src/components/table-record/TableRecord.jsx
+++ src/components/table-record/TableRecord.jsx
@@ -3,11 +3,33 @@ import './table-record.scss'
 import edit from '../../assets/img/edit-icon.svg'
 import del from '../../assets/img/delete-icon.svg'
 
-function TableRecord() {
+function TableRecord({recordData}) {
 
   return (
     <div className="table-record">
-        TableRecord
+        {console.log('Rendered TableRecord')}
+        <div className="table-record__record-info">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>{recordData.date}</td>
+                        <td>{recordData.situation}</td>
+                        <td>{recordData.thoughts}</td>
+                        <td>{recordData.emotions}</td>
+                        <td>{recordData.bodyReaction}</td>
+                        <td>{recordData.behavior}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div className="table-record__actions">
+            <div className="table-record__edit-btn">
+                <img src={edit} alt="edit-icon" />
+            </div>
+            <div className="table-record__delete-btn">
+                <img src={del} alt="delete-icon" /> 
+            </div>
+        </div>
     </div>
   );
 }
--- src/views/my-diary/MyDiary.jsx
+++ src/views/my-diary/MyDiary.jsx
@@ -5,6 +5,36 @@ import add from '../../assets/img/add-button.svg'
 
 function MyDiary() {
 
+  const data = [
+    {
+      id: 1, 
+      date: '08.10.2023',
+      situation: 'Сижу в школе, много мыслей в голове',
+      thoughts: 'Я никому не нужна, никто меня не любит и не ценит. Почему так????? Чем я это заслужила',
+      emotions: '%emotion%',
+      bodyReaction: 'Грызу губы',
+      behavior: 'Стучу ручкой по столу',
+    },
+    {
+      id: 2, 
+      date: '09.10.2023',
+      situation: 'Гуляли с подругой',
+      thoughts: 'Как хорошо быть рядом с человеком, который тебя понимает',
+      emotions: '%emotion%',
+      bodyReaction: 'Учащенное сердцебиение',
+      behavior: 'Много улыбалась',
+    },
+    {
+      id: 3, 
+      date: '11.10.2023',
+      situation: 'Поссорились с мамой из-за выгула собаки',
+      thoughts: 'Можно не трогать меня хотя бы пять минут',
+      emotions: '%emotion%',
+      bodyReaction: 'Сокращение мышц рук и ног',
+      behavior: 'Закрылась в комнате и плакала',
+    },
+  ]
+
   return (
     <div className="my-diary">
       {console.log('Rendered MyDiary view')}
@@ -36,9 +66,13 @@ function MyDiary() {
                 </div>
             </div>
             <div className="my-diary__table-content">
-              <TableRecord />
-              <TableRecord />
-              <TableRecord />
+              {
+                data.toReversed().map((dataItem) => (
+                  <div key={dataItem.id}>
+                    <TableRecord recordData={dataItem} />
+                  </div>
+                ))
+              }
             </div>
           </div>
         </div>
